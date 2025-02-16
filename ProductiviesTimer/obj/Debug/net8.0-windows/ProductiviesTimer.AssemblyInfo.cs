f7dd6a128d95f5b6c0decf7b37b4439d0558b25b
Melicharik <lilithium06@gmail.com>
2025-02-16 19:11:54 +0100

    Added a field to change the amount of money per time

--- ProductiviesTimer/obj/Debug/net8.0-windows/ProductiviesTimer.AssemblyInfo.cs
+++ ProductiviesTimer/obj/Debug/net8.0-windows/ProductiviesTimer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductiviesTimer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c4b957b64686353dc7fc6de5e19195847bfb3de")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductiviesTimer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductiviesTimer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
